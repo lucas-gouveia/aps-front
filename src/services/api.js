a640e24d27c4c7f9dcc343ca8a6cb0bfa1c03835
Lucas Gouveia <56007090+lucas-gouveia@users.noreply.github.com>
2021-12-01 21:49:15 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://c955-2804-14d-3281-904d-f0ba-1d93-f24b-5da8.ngrok.io/',
+  baseURL: 'http://localhost',
 })
 
 export default api;
